4dfe4f21c642ba011075ca065c4d7b9c2e33c600
Ken <knel.tw@gmail.com>
2016-11-25 18:40:48 +0800

    Red

--- tests/DetectionTest.php
+++ tests/DetectionTest.php
@@ -34,4 +34,35 @@ class DetectionTest extends TestCase
 
         $this->assertEquals($expected, $result);
     }
+
+    public function testShouldBeOnePair()
+    {
+        $expected = 'OnePair';
+        $detection = new \PokerDetection\Detection();
+        $cards = [
+            [
+                'number' => 'A',
+                'suits' => 'Diamonds',
+            ],
+            [
+                'number' => 'A',
+                'suits' => 'Spades',
+            ],
+            [
+                'number' => '5',
+                'suits' => 'Hearts',
+            ],
+            [
+                'number' => '7',
+                'suits' => 'Clubs',
+            ],
+            [
+                'number' => '9',
+                'suits' => 'Diamonds',
+            ],
+        ];
+        $result = $detection->detect($cards);
+
+        $this->assertEquals($expected, $result);
+    }
 }
